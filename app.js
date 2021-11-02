0e6996dcc09478770bef0651d4ad752fa9d2c731
NicolaL95 <nicola.lilla@gmail.com>
2021-11-02 17:05:18 +0100

    Aggiunta struttura e gestine iniziale multi list

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const app = new Vue({
     data: {
         addedItems: "",
         error: false,
-        lists: ["aggiornare il framework", "completare il front-end", "salvare la cheerleader per salvare il mondo"]
+        lists: ["aggiornare il framework", "completare il front-end", "salvare la cheerleader per salvare il mondo"],
+        listTrash: [],
+        listCompl: []
     },
     methods: {
-        removeList_I(i) {
+        removeList_I(i, c_o_r) {
+            if (c_o_r == 1) {
+                this.listCompl.push(this.lists[i])
+            }
+            else {
+                this.listTrash.push(this.lists[i])
+            }
             this.lists.splice(i, 1);
+            console.log(this.listTrash)
         },
-        addList_I() {
+        addItem() {
             if (this.addedItems.length >= 5) {
                 this.lists.push(this.addedItems);
                 this.error = false;
@@ -19,6 +28,9 @@ const app = new Vue({
                 this.error = true;
             }
             this.addedItems = "";
+        },
+        addList_I() {
+
         }
     }
 })
