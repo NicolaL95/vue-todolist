bfa8c423b208df141b9f4ff9f39b2bcb813724a0
NicolaL95 <nicola.lilla@gmail.com>
2021-11-02 15:25:13 +0100

    Completament lavoro base

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const app = new Vue({
     el: "#app",
+
     data: {
+        addedItems: "",
         lists: ["aggiornare il framework", "completare il front-end", "salvare la cheerleader per salvare il mondo"]
     },
     methods: {
         removeList_I(i) {
             this.lists.splice(i, 1);
+        },
+        addList_I() {
+            if (this.addedItems.length >= 5) {
+                this.lists.push(this.addedItems);
+            }
+            else {
+                alert("cia fraa")
+            }
+            this.addedItems = "";
         }
     }
 })
